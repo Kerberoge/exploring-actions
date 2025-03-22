410268e1d0dd2b85c1ba51a96cfb039542c807e1
kerberoge <sjoerdenjh@gmail.com>
2025-03-23 00:07:29 +0100

    Added libelf-dev to package list

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@
 
 VERSION=$(grep "Linux/x86 [0-9.]* Kernel Configuration" .config | cut -d' ' -f3)
 
+sudo apt-get install -qq libelf-dev
+
 curl -LO https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$VERSION.tar.xz
 tar -xf linux-$VERSION.tar.xz
 rm linux-$VERSION.tar.xz
