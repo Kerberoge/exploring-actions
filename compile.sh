28ec132acbddca7fdb6d8e68ef242a63a755aa65
kerberoge <sjoerdenjh@gmail.com>
2025-03-23 00:27:49 +0100

    Added step for installing and unpacking required firmware files in workflow file

--- compile.sh
+++ compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ls /lib/firmware/rtw88
-
 VERSION=$(grep "Linux/x86 [0-9.]* Kernel Configuration" .config | cut -d' ' -f3)
 
-sudo apt-get install -qq libelf-dev
+sudo apt-get install -qq libelf-dev linux-firmware
+
+# Extract firmware blobs
+for file in $(grep EXTRA_FIRMWARE= .config | sed -E 's/.*"(.*)"/\1/'); do
+	zstd -d /lib/firmware/$file.zst
+done
 
 curl -LO https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$VERSION.tar.xz
 tar -xf linux-$VERSION.tar.xz
